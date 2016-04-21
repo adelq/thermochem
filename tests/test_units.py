4fb126509d3f8f457082201ad541aa10813d61d2
Adel Qalieh <aqalieh95@gmail.com>
2016-04-21 18:15:05 -0400

    Add tests for units

---
+++ tests/test_units.py
@@ -0,0 +1,10 @@
+from thermopy.units import Pressure, Temperature
+from nose.tools import assert_equals
+
+
+def test_temperature():
+    assert_equals(Temperature(273.15).unit('K'), Temperature(0).unit('C'))
+
+
+def test_pressure():
+    assert_equals(Pressure(760).unit('torr'), Pressure(1).unit('atm'))
