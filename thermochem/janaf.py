84fa40f09febf89f0be6d2b63439d817777d23ae
Adel Qalieh <aqalieh95@gmail.com>
2016-04-23 12:47:48 -0400

    Reformat file to be consistent with PEP8

--- thermochem/janaf.py
+++ thermochem/janaf.py
@@ -1,11 +1,11 @@
-# janaf.py
+"""
+This module gets thermodynamic datat from the JANAF database.
+Files are downloaded from the NIST servers as needed and then cached locally.
 
-# This module gets thermodynamic datat from the JANAF database.
-# Files are downloaded from the NIST servers as needed and then cached locally.
-#
-# Zack Gainsforth
-#
-# Funding by NASA
+Zack Gainsforth
+
+Funding by NASA
+"""
 
 from __future__ import division
 from __future__ import print_function
@@ -29,16 +29,16 @@ except ImportError:
     # Python 3
     from io import StringIO
 
-
 # Universal gas constant R
 R = 8.314472
+
 
 class JanafPhase(object):
     """
     Class which is created by Janafdb for a specific phase.
 
-    It reads in the JANAF data file and produces functions which interpolate the thermodynamic
-    constants.
+    It reads in the JANAF data file and produces functions which interpolate
+    the thermodynamic constants.
 
     >>> db = Janafdb()
     >>> p = db.getphasedata(formula='O2Ti', name='Rutile', phase='cr')
@@ -61,19 +61,28 @@ class JanafPhase(object):
         ...
     ValueError: A value in x_new is above the interpolation range.
     """
+
     def __init__(self, rawdata_text):
         # Store the raw data text file from NIST.
         self.rawdata_text = rawdata_text
 
         # Read the text file into a DataFrame.
-        data = pd.read_csv(StringIO(self.rawdata_text), skiprows=2, header=None, delimiter='[\t\s]*', engine='python')
-        data.columns = ['T', 'Cp', 'S', '[G-H(Tr)]/T', 'H-H(Tr)', 'Delta_fH', 'Delta_fG', 'log(Kf)']
+        data = pd.read_csv(
+            StringIO(self.rawdata_text),
+            skiprows=2,
+            header=None,
+            delimiter='[\t\s]*',
+            engine='python')
+        data.columns = ['T', 'Cp', 'S', '[G-H(Tr)]/T', 'H-H(Tr)', 'Delta_fH',
+                        'Delta_fG', 'log(Kf)']
         self.rawdata = data
 
-        # Sometimes the JANAF files have funky stuff written in them.  (Old school text format...)
+        # Sometimes the JANAF files have funky stuff written in them. (Old
+        # school text format...)
         # Clean it up.
         for c in data.columns:
-            # We only need to polish up columns that aren't floating point numbers.
+            # We only need to polish up columns that aren't floating point
+            # numbers.
             if np.issubdtype(data.dtypes[c], np.floating):
                 continue
             # Change INFINITE to inf
@@ -95,22 +104,28 @@ class JanafPhase(object):
         self.DeltaG = interp1d(self.rawdata['T'], self.rawdata['Delta_fG'])
         self.logKf = interp1d(self.rawdata['T'], self.rawdata['log(Kf)'])
 
-        # TODO Deal well with crystal<->liquid transitions which have a below and above value for Cp, S, etc.
+        # TODO Deal well with crystal<->liquid transitions which have a below
+        # and above value for Cp, S, etc.
+
 
 class Janafdb(object):
-
     """
     Class that reads the NIST JANAF tables for thermodynamic data.
 
     Data is initially read from the web servers, and then cached.
     """
+    VALIDPHASETYPES = ['cr', 'l', 'cr,l', 'g', 'ref', 'cd', 'fl', 'am', 'vit',
+                       'mon', 'pol', 'sln', 'aq', 'sat']
+    JANAF_URL = "http://kinetics.nist.gov/janaf/html/%s.txt"
 
     def __init__(self):
         """
-        We have an index file which can be used to build the url for all phases on the NIST site.
+        We have an index file which can be used to build the url for all phases
+        on the NIST site.
         """
 
-        # Read the index file which tells us the filenames for all the phases in the JANAF database.
+        # Read the index file which tells us the filenames for all the phases
+        # in the JANAF database.
         self.db = pd.read_csv("thermochem/JANAF_index.txt", delimiter='|')
         # Name the columns and trim whitespace off the text fields.
         self.db.columns = ['formula', 'name', 'phase', 'filename']
@@ -159,20 +174,21 @@ class Janafdb(object):
         >>> db.getphasedata(formula='Oxyz')
         Traceback (most recent call last):
             ...
-        ValueError: Valid phase types are ['cr', 'l', 'cr,l', 'g', 'ref', 'cd', 'fl', 'am', 'vit', 'mon', 'pol', 'sln', 'aq', 'sat'].
+        ValueError: Valid phase types are ['cr', 'l', 'cr,l', 'g', 'ref', 'cd',
+        'fl', 'am', 'vit', 'mon', 'pol', 'sln', 'aq', 'sat'].
         >>> db.getphasedata(formula='Oxyz', phase='l')
         Traceback (most recent call last):
             ...
         ValueError: Did not find Oxyz, None, (l)
-
         """
 
         # Check that the phase type requested is valid.
-        validphasetypes = ['cr', 'l', 'cr,l', 'g', 'ref', 'cd', 'fl', 'am', 'vit', 'mon', 'pol', 'sln', 'aq', 'sat']
-        if phase not in validphasetypes:
-            raise ValueError("Valid phase types are " + str(validphasetypes) + ".")
+        if phase not in Janafdb.VALIDPHASETYPES:
+            raise ValueError("Valid phase types are " + str(
+                Janafdb.VALIDPHASETYPES) + ".")
 
-        # We can search on either an exact formula, partial text match in the name, and exact phase type.
+        # We can search on either an exact formula, partial text match in the
+        # name, and exact phase type.
         formulasearch = pd.Series(np.ones(len(self.db)), dtype=bool)
         namesearch = formulasearch.copy()
         phasesearch = formulasearch.copy()
@@ -187,23 +203,28 @@ class Janafdb(object):
         # Get the record (should be one record) which specifies this phase.
         PhaseRecord = self.db[searchmatch]
         if len(PhaseRecord) == 0:
-            raise ValueError("Did not find %s, %s, (%s)" % (formula, name, phase))
+            raise ValueError("Did not find %s, %s, (%s)" %
+                             (formula, name, phase))
         if len(PhaseRecord) > 1:
-            raise ValueError("There are %d records matching this pattern."%len(PhaseRecord))
+            raise ValueError("There are %d records matching this pattern." %
+                             len(PhaseRecord))
 
         # At this point we have one record.  Check if we have that file cached.
-        cachedfilename = os.path.join(self.JANAF_cachedir, PhaseRecord['filename'].values[0]+'.txt')
-        if os.path.exists(cachedfilename) and nocache==False:
+        cachedfilename = os.path.join(
+            self.JANAF_cachedir, PhaseRecord['filename'].values[0] + '.txt')
+        if os.path.exists(cachedfilename) and not nocache:
             # Yes it was cached, so let's read it into memory.
             with open(cachedfilename, 'r') as f:
                 textdata = f.read()
         else:
             # No it was not cached so let's get it from the web.
-            response = urllib2.urlopen('http://kinetics.nist.gov/janaf/html/%s.txt'%PhaseRecord['filename'].values[0])
+            response = urllib2.urlopen(Janafdb.JANAF_URL %
+                                       PhaseRecord['filename'].values[0])
             textdata = response.read()
 
-            # And cache the data so we aren't making unnecessary trips to the web.
-            if nocache==False:
+            # And cache the data so we aren't making unnecessary trips to the
+            # web.
+            if not nocache:
                 with open(cachedfilename, 'w') as f:
                     f.write(textdata)
 
