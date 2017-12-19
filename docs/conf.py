b3a0a7cc0f5e2803df5a4c91535c6b88f81a923d
Adel Qalieh <aqalieh95@gmail.com>
2017-12-18 19:37:13 -0500

    Add MathJax formula rendering

--- docs/conf.py
+++ docs/conf.py
@@ -31,10 +31,13 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
+extensions = [
+    'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.coverage',
-    'sphinx.ext.viewcode']
+    'sphinx.ext.viewcode',
+    'sphinx.ext.mathjax',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
