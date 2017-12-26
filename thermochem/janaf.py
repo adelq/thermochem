a8907cd492356ec74df248d85fd6f1ae0a1ce170
Adel Qalieh <aqalieh95@gmail.com>
2017-12-25 19:28:07 -0500

    Use .empty method to avoid condition on length

--- thermochem/janaf.py
+++ thermochem/janaf.py
@@ -204,7 +204,7 @@ class Janafdb(object):
 
         # Get the record (should be one record) which specifies this phase.
         phase_record = self.db[searchmatch]
-        if len(phase_record) == 0:
+        if phase_record.empty:
             raise ValueError("Did not find %s, %s, (%s)" %
                              (formula, name, phase))
         if len(phase_record) > 1:
