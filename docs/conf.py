79413e4e4ddfebed0e4d98061cbdf5daadddd80f
Adel Qalieh <aqalieh95@gmail.com>
2020-05-18 18:58:47 -0400

    Add napoleon sphinx extension

--- docs/conf.py
+++ docs/conf.py
@@ -37,6 +37,7 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
     'sphinx.ext.mathjax',
+    'sphinx.ext.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
